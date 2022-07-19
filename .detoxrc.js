61018f96118f110712c3eb39afa48f40e3c70ace
Ioana Badina <ioana.badina@yahoo.com>
2022-07-19 17:00:49 +0300

    configure detox

---
+++ .detoxrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "configurations": {
+    "ios.sim.debug": {
+      "binaryPath": "ios/build/Build/Products/Debug-iphonesimulator/pqaa_detox.app",
+      "build": "xcodebuild -workspace ios/pqaa_detox.xcworkspace -scheme pqaa_detox -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
+      "type": "ios.simulator",
+      "device": {
+        "type": "iPhone 12 Pro",
+      }
+    },
+    "android.emu.debug": {
+      "binaryPath": "android/app/build/outputs/apk/debug/app-debug.apk",
+      "build": "cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..",
+      "type": "android.emulator",
+      "device": {
+        "avdName": "Pixel_2_API_29",
+      }
+    }
+  }
+}
---
+++ cucumber.js
@@ -0,0 +1,8 @@
+const arguments = [
+  '--require-module @babel/register',
+  'e2e/features/*/*.feature'
+].join(' ');
+
+module.exports = {
+  default: arguments
+}
