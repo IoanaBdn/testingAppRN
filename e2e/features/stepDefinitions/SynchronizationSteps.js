bccbf781cc9fbc405d9193da913350ddb68e82a9
Ioana Badina <ioana.badina@yahoo.com>
2022-08-11 00:15:36 +0300

    expect toggle value

--- e2e/features/stepDefinitions/SynchronizationSteps.js
+++ e2e/features/stepDefinitions/SynchronizationSteps.js
@@ -2,4 +2,9 @@ import {When} from '@cucumber/cucumber';
 
 When('I toggle the animation\'s switch', async()=>{
     await element(by.id('animationSwitch')).tap();
+    await expect(element(by.id('animationSwitch'))).toHaveToggleValue(true);
+    await element(by.id('animationSwitch')).tap();
+    await expect(element(by.id('animationSwitch'))).toHaveToggleValue(false);
+
+
 });
