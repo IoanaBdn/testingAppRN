91adf188483c168de44d3c5568512bc61100b7eb
Ioana Badina <ioana.badina@yahoo.com>
2022-08-11 15:30:46 +0300

    release builds

--- .detoxrc.js
+++ .detoxrc.js
@@ -11,6 +11,17 @@ module.exports = {
               "rootDir": "e2e/reports/artifacts/ios",
           }
       },
+      "ios.sim.release": {
+        "binaryPath": "ios/build/Build/Products/Release-iphonesimulator/pqaa_detox.app",
+        "build": "ENVFILE=.env.testing xcodebuild -workspace ios/pqaa_detox.xcworkspace -scheme pqaa_detox -configuration Release -sdk iphonesimulator -derivedDataPath ios/build",
+        "type": "ios.simulator",
+        "device": {
+            "type": "iPhone 12 Pro"
+        },
+        "artifacts": {
+            "rootDir": "e2e/reports/artifacts/ios",
+        }
+    },
       "android.emu.debug": {
           "binaryPath": "android/app/build/outputs/apk/debug/app-debug.apk",
           "build": "cd android && ENVFILE=.env.testing ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..",
@@ -21,6 +32,17 @@ module.exports = {
           "artifacts": {
               "rootDir": "e2e/reports/artifacts/android",
           }
-      }
+      },
+      "android.emu.release": {
+        "binaryPath": "android/app/build/outputs/apk/release/app-release.apk",
+        "build": "cd android && ENVFILE=.env.testing ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..",
+        "type": "android.emulator",
+        "device": {
+            "avdName": "Pixel_XL"
+        },
+        "artifacts": {
+            "rootDir": "e2e/reports/artifacts/android",
+        }
+    }
   }
 }
